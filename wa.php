91a8c3d6bbbe43a6ea9242ef18bb9a39c7941e11
MRram0s <50142183+MRram0s@users.noreply.github.com>
2019-05-02 18:12:08 +0800

    Update wa.php

--- wa.php
+++ wa.php
@@ -4,7 +4,7 @@ echo "Nomor Target?\nInput : ";
 $nomer = trim(fgets(STDIN));
 if(strlen($nomer)==8){
 	$nomer = str_replace("60".$nomer);
-}elseif(strlen($nomer)>10){
+}elseif(strlen($nomer)>9){
 	$nomer = str_replace("60",$nomer);
 }
 echo "Target: $nomer (y/n)";
