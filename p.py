63362710025e6e542250bb5e5847cc95356ca4ba
MRram0s <50142183+MRram0s@users.noreply.github.com>
2019-05-04 04:25:48 +0800

    Update p.py

--- p.py
+++ p.py
@@ -11,6 +11,15 @@ except ModuleNotFoundError:
 	print("\nSepertinya module requests BELUM Di Install")
 	print("$ pip install requests\n")
 	exit()
+	unitColor = '\033[5;36m\033[5;47m'
+endColor = '\033[0;0m\033[0;0m'
+count = 45
+for i in range(count):
+    incre = int(50.0 / count * i)
+    sys.stdout.write('\r' + '|%s%s%s%s| %d%%' % (unitColor, '\033[7m' + ' '*incre + ' \033[27m', endColor, ' '*(50-incre), 2*incre)) if i != count — 1 else sys.stdout.write('\r' + '|%s%s%s| %d%%' % (unitColor, '\033[7m' + ' '*20 + 'COMPLETE!' + ' '*21 + ' \033[27m’, endColor, 100))
+    sys.stdout.flush()
+    sleep(0.1)
+sys.stdout.write('\n')
 
 os.system('clear')
 c=('\033[1;36m')
