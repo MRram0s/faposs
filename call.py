8b7ccd9e66e0c2c26c3ae1bcdc2f8dd3066f1785
MRram0s <50142183+MRram0s@users.noreply.github.com>
2019-05-03 17:30:37 +0800

    Update call.py

--- call.py
+++ call.py
@@ -31,7 +31,7 @@ print("""%s
 /| |   | |\_//	%sMASUKKAN NOMBOR DENGAN "60" (EX: 601XXXXXXX)%s
 \| |._.| |/-’
  '"'   '"'
-<NOTE> Kalu ERROR @ BUG dan lain-lain, silalah hubungi bapak saya.."""%(c,r,g,r,g,r,g,r,g,r,w,r))
+<NOTE> Kalu ERROR @ BUG dan lain-lain, silalah hubungi bapak saya.."""%(c,r,g,r,g,r,g,r,g,r,w,o))
 print("%s[*] Klik ENTER untuk step seterusnya%s"%(g,g))
 no1 = input("[?] NO.TARGET 1 => %s"%(w))
 no2 = input("%s[?] NO.TARGET 2 => %s"%(g,w))
