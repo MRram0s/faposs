c9e96b92a53d5ed0f47b96aea5343056c903ba64
MRram0s <50142183+MRram0s@users.noreply.github.com>
2019-05-04 11:27:44 +0800

    Update p.py

--- p.py
+++ p.py
@@ -91,7 +91,7 @@ try:
 			print("%s[+] TARGET5   ✔️"%(g))
 		else:
 			print("%s[-] TARGET5   ❌"%(o))
-		print("="*30)
+		print("%s="*30%(c))
 		time.sleep(1)
 except KeyboardInterrupt:
 	print("%sterima kasih bossku..."%(c))
